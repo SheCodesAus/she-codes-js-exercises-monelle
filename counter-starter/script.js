022ad6aed326c019c6f41660ff2e1c965eaafbf2
Monelle Trachez <monelle.trachez@gmail.com>
2022-04-01 16:13:46 +0800

    Counter-Starter exercises Finished

--- counter-starter/script.js
+++ counter-starter/script.js
@@ -1,5 +1,4 @@
 function add(step) {
-    // console.log("dis will add one");
     let numberElement = document.getElementById("number");
     let number = numberElement.innerText;
     number = parseInt(number) + step;
@@ -8,10 +7,23 @@ function add(step) {
 }
 
 function substract(step) {
-    // console.log("dis will substract one");
     let numberElement = document.getElementById("number");
     let number = numberElement.innerText;
     number = parseInt(number) - step;
+    numberElement.innerText = number;
+    console.log(number);
+}
+// Define function to apply a value from TextField to our counter:
+function applyNumberInput() {
+    // Point to the HTMLelement where to find the value:
+    let numberInputElement = document.getElementById("number-input");
+    // .value lifts the actual value from Element; then assigns value to 'step' variable:
+    let step = numberInputElement.value || 0;
+    // Proceed with the original function
+    let numberElement = document.getElementById("number");
+    let number = numberElement.innerText;
+    // Add 'parseInt' to 'step' as value is a string:
+    number = parseInt(number) + parseInt(step);
     numberElement.innerText = number;
     console.log(number);
 }
