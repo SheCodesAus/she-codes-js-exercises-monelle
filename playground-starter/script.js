558a64f9c9c55eccdfb45877f2835c874a2d98ea
Monelle Trachez <monelle.trachez@gmail.com>
2022-04-05 15:26:28 +0800

    Completed exercises

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -1,6 +1,45 @@
-console.log("Hello, world!");
+// console.log("Hello, world!");
 
 function showName() {
     let username = document.getElementById("name-text").value;
     console.log(username);
 }
+
+
+// for (let count = 0; count < 10; count++) {
+//     console.log(count)
+// }
+
+// let pets = ["Chilli", "Rex", "Roary", "Fleur"];
+// console.log(pets);
+
+// pets.push("Ivy");
+// console.log(pets);
+// console.log(pets[0]);
+// console.log(pets[3]);
+
+// console.log("------------------")
+
+// for (let index = 0; index < pets.length; index ++) {
+//     console.log(pets[index]);
+// }
+
+// console.log("------------------")
+
+// // 'forEach' is a predetermined function
+// pets.forEach((pet) => {
+//     console.log(pet);
+// });
+
+
+let pet = "Chilli"
+
+if (pet == "Chilli") {
+    console.log("nfewjcfbwckj");
+} else if (pet == "IVy") {
+    console.log("eyahf whatever");
+} else {
+    console.log("fwjcfnwjlcfwp");
+}
+
+// can stack up as many conditions as needed. the middle ones will be else if, else if etc, last else.
--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,3 +1,39 @@
-function addItem() {}
+// Write a loop to add items to the list
+let shoppingListItems = ["milk", "eggs", "bread"];
+// Assign the element 'list' to a variable called 'listElement'
+let listElement = document.getElementById("shopping-list-items");
 
-function updateItems() {}
+shoppingListItems.forEach((item) => {
+    console.log(item);
+    let itemElement = document.createElement("li");
+    itemElement.innerText = item;
+    listElement.appendChild(itemElement);
+});
+
+// After adding 'onclick' feat to Submit button in HTML, write function to add items when clicking
+function addItem() {
+    let item = document.getElementById("new-item-text").value;
+    shoppingListItems.push(item);
+    console.log(shoppingListItems);
+    updateItems()
+}
+
+function updateItems() {
+    let listElement = document.getElementById("shopping-list-items");
+    listElement.innerHTML = "";
+    shoppingListItems.forEach((item) => {
+        let itemElement = document.createElement("li");
+        itemElement.innerText = item;
+        listElement.appendChild(itemElement);
+    });
+}
+
+function clearList() {
+    shoppingListItems = [];
+    // alternatively:
+    //shoppingListItems.length = 0; - instead of line 35
+    updateItems();
+}
+
+
+
